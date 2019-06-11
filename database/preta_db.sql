d33ebdfdcef040218bd40ae2645bc2ad2cecd775
ornella <ornella.doricelli@gmail.com>
2019-06-10 23:00:28 -0300

    database para importar

--- database/preta_db.sql
+++ database/preta_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 31-05-2019 a las 03:07:50
+-- Tiempo de generación: 11-06-2019 a las 03:59:45
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.2
 
@@ -82,23 +82,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_05_23_111207_create_categories_table', 1),
 (4, '2019_05_23_135406_create_products_table', 1),
-(6, '2019_05_23_140001_create_carts_table', 2),
-(7, '2019_05_30_235743_create_orders_table', 3);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `orders`
---
-
-CREATE TABLE `orders` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `user_id` bigint(20) UNSIGNED NOT NULL,
-  `price` decimal(10,2) NOT NULL,
-  `order_number` int(11) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+(6, '2019_05_23_140001_create_carts_table', 2);
 
 -- --------------------------------------------------------
 
@@ -137,17 +121,18 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `price`, `description`, `discount`, `photo1`, `photo2`, `photo3`, `category_id`, `created_at`, `updated_at`) VALUES
-(3, 'Top MANDY', '600.00', 'Top acanalado con botones al frente, de morley blanco.', NULL, '/images/products/tops/top_acanalado_front_white.jpg', '/images/products/tops/top_acanalado_back_white.jpg', '/images/products/tops/top_acanalado_detail_white.jpg', 2, '2019-05-29 03:00:00', NULL),
-(4, 'Top MANDY', '600.00', 'Top acanalado con botones al frente, de morley camel', 10, '/images/products/tops/top_acanalado_front_camel.jpg', '/images/products/tops/top_acanalado_back_camel.jpg', '/images/products/tops/top_acanalado_detail_camel.jpg', 2, '2019-05-29 03:00:00', NULL),
-(5, 'Top LEIA', '720.00', 'Remera de rayon negro con volados en las mangas', NULL, '/images/products/tops/top_volados_front_black.jpg', '/images/products/tops/top_volados_back_black.jpg', '/images/products/tops/top_volados_detail_black.jpg', 2, '2019-05-01 03:00:00', NULL),
-(6, 'Top LEIA', '720.00', 'Remera de rayon blanco con rayas con volados en las mangas', NULL, '/images/products/tops/top_volados_front_stripes.jpg', '/images/products/tops/top_volados_back_stripes.jpg', '/images/products/tops/top_volados_detail_stripes.jpg', 2, '2019-05-07 03:00:00', NULL),
+(3, 'Top MANDY', '600.00', 'Top acanalado con botones al frente, de morley blanco.', NULL, '8dK1lP5WMOcS5dBKNL6selr1WAUziVgKF7RDSxUQ.jpeg', '2qaHmkWq2IGJwbsS3mBczjLRpUA0avYNH4OKCbZe.jpeg', '1wb17mR7tl93Upwhg0ig4UU4o47EyrSx5GEbILgd.jpeg', 2, '2019-05-29 03:00:00', '2019-06-11 04:33:53'),
+(4, 'Top MANDY', '600.00', 'Top acanalado con botones al frente, de morley camel', 10, 'HdmVSbRn4WcjqPn0W4h6Kex9cnHUytcgNN2s8cxv.jpeg', 'QqAshMvgMxzUFt3urNZYQNPUwumEjIILGVlDcVvO.jpeg', 'bpDoLMuuLY5jE6Lqc4EhLSNSGBkOCGZf5EQcILGU.jpeg', 2, '2019-05-29 03:00:00', '2019-06-11 04:34:29'),
+(5, 'Top LEIA', '720.00', 'Remera de rayon negro con volados en las mangas', NULL, 'zZ5lp6nq0zoUoXwdqUj0ZHgcHmi0g6dXhJUsIcWG.jpeg', 'Nz7IUP42tBKxuoiVOVK0bia6KmudGhYiXnAmPEge.jpeg', 'CBWcQGPSqikZMgcbfQ2QNpHQT7Lwg5ngAJfcot1k.jpeg', 2, '2019-05-01 03:00:00', '2019-06-11 04:35:01'),
+(6, 'Top LEIA', '720.00', 'Remera de rayon blanco con rayas con volados en las mangas', NULL, 'm9gkXtg8nsD65enugcOCjgTzsMI1bAbdOeWyMVpT.jpeg', 'Vg5DpC6U4DDtJJww4sA0XcvK9zz5Tvj2dT3EIWGu.jpeg', 'yyyDAOmYG3rcwSy7HfiUyodMrcO2phPp4lLshAgo.jpeg', 2, '2019-05-07 03:00:00', '2019-06-11 04:35:30'),
 (7, 'Vestido COACHELA', '1490.00', 'Solera ajustada a la cintura con estampa floreada. ', NULL, '/images/products/dresses/dress_floreado_front_yellow.jpg', '/images/products/dresses/dress_floreado_back_yellow.jpg', '/images/products/dresses/dress_floreado_detail_yellow.jpg', 4, '2019-05-29 03:00:00', NULL),
 (8, 'Vestido GRECIA', '2590.00', 'Vestido de gasa plisada celeste, escote halter con abertura en pecho y espalda.', NULL, '/images/products/dresses/dress_plisado_front_blue.jpg', '/images/products/dresses/dress_plisado_back_blue.jpg', '/images/products/dresses/dress_plisado_detail_blue.jpg', 4, '2019-04-23 03:00:00', NULL),
 (9, 'Buzo BRADLY', '990.00', 'Buzo cropped de algodon negro con cordon en escote', 20, '/images/products/outerwear/hoodie_cruzado_front_black.jpg', '/images/products/outerwear/hoodie_cruzado_back_black.jpg', '/images/products/outerwear/hoodie_cruzado_detail_black.jpg', 3, '2019-05-29 03:00:00', NULL),
 (10, 'Parka ARMY', '2800.00', 'Parka de gabardina verde militar', NULL, '/images/products/outerwear/jacket_parka_front_green.jpg', '/images/products/outerwear/jacket_parka_back_green.jpg', '/images/products/outerwear/jacket_parka_detail_green.jpg', 3, '2019-05-01 03:00:00', NULL),
 (11, 'Pantalon WIDE', '1650.00', 'Jean negro pierna ancha, tiro extra alto', NULL, '/images/products/denim/denim_ancho_front_black.jpg', '/images/products/denim/denim_ancho_back_black.jpg', '/images/products/denim/denim_ancho_detail_black.jpg', 1, '2019-05-01 03:00:00', NULL),
 (12, 'Jean BOYFRIEND', '1890.00', 'Jean rigido corte boyfriend, roturas al frente', NULL, '/images/products/denim/denim_boyfriend_front_blue.jpg', '/images/products/denim/denim_boyfriend_back_blue.jpg', '/images/products/denim/denim_boyfriend_detail_blue.jpg', 1, '2019-05-29 03:00:00', NULL),
-(13, 'Pantalon SNOW', '1300.00', 'Jean skinny blanco elastizado', NULL, '/images/products/denim/denim_skinny_front_white.jpg', '/images/products/denim/denim_skinny_back_white.jpg', '/images/products/denim/denim_skinny_detail_white.jpg', 1, '2019-05-01 03:00:00', NULL);
+(13, 'Pantalon SNOW', '1300.00', 'Jean skinny blanco elastizado', NULL, '/images/products/denim/denim_skinny_front_white.jpg', '/images/products/denim/denim_skinny_back_white.jpg', '/images/products/denim/denim_skinny_detail_white.jpg', 1, '2019-05-01 03:00:00', NULL),
+(14, 'Campera SHINE', '3499.00', 'Chaqueta de denim con bordado de lentejuelas en frente y espalda.', NULL, 'UnkWIpAF0RaKHEsNCeTG7AcgdEb28tTlHzsxYKT7.jpeg', 'BHGLoeQz7DY50ZwP2U0xkBPKOypwHVP1uaA873J9.jpeg', 'PYlqreO6BfLLnuACGggJfDp4x2JONzuWqEcsvYC1.jpeg', 3, '2019-06-11 04:15:37', '2019-06-11 04:15:37');
 
 -- --------------------------------------------------------
 
@@ -195,13 +180,6 @@ ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `orders_user_id_foreign` (`user_id`);
-
---
 -- Indices de la tabla `password_resets`
 --
 ALTER TABLE `password_resets`
@@ -244,16 +222,10 @@ ALTER TABLE `migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT de la tabla `orders`
---
-ALTER TABLE `orders`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de la tabla `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
@@ -271,12 +243,6 @@ ALTER TABLE `users`
 ALTER TABLE `carts`
   ADD CONSTRAINT `carts_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
   ADD CONSTRAINT `carts_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
---
--- Filtros para la tabla `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Filtros para la tabla `products`
