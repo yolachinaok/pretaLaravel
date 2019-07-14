6617c4641c068475f70fb8fc8de5a9a6a30b62bf
ornella <ornella.doricelli@gmail.com>
2019-07-14 17:51:17 -0300

    correccion migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('avatar',300)->nullable();
             $table->string('password');
-            $table->integer('esAdmin');
+            $table->integer('esAdmin')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
